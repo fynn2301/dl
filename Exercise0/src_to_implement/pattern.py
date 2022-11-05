d0dcb0cbdb8d8d052588da8f864fb537f5dd2443
Fynn-Lennardt Metzler <fynn-lennardt.metzler@fau.de>
2022-11-05 14:20:59 +0100

    fix: deleted one circle

--- Exercise0/src_to_implement/pattern.py
+++ Exercise0/src_to_implement/pattern.py
@@ -140,46 +140,6 @@ class Circle():
         plt.axis('off')
         plt.show()
 
-class Circle():
-    """Creates a Circle pattern
-    """
-
-    def __init__(self, resolution: int, radius: int, position:tuple) -> None:
-        """Initilizes the class Checker
-        """
-        self.resolution = resolution
-        self.radius = radius
-        self.position = position
-        self.output = np.ndarray((resolution, resolution))
-
-    def draw(self) -> np.ndarray:
-        """Creating the array with the circle
-
-        Returns:
-            np.ndarray: the created array
-        """
-        
-        a = np.arange(self.resolution)  
-        b = np.arange(self.resolution)  
-        x, y = np.meshgrid(a, b, sparse=True)
-        
-        # x = array % self.resolution
-        # y = array // self.resolution
-        # sqrt(pow(x - x_0,2), pow(y - y_0,2)) < radius  -> the value is inside the radius and it should be white
-        x_0 = self.position[0]
-        y_0 = self.position[1]
-        array = np.where(np.sqrt(np.power(x - x_0, 2) + np.power(y - y_0, 2)) <= self.radius, 1, 0)
-        
-        self.output = array
-        return copy.copy(array)
-
-    def show(self) -> None:
-        """Shows the pattern
-        """
-        plt.imshow(self.output, cmap='gray')
-        plt.axis('off')
-        plt.show()
-
 class Spectrum():
     """Creates a Circle pattern
     """
