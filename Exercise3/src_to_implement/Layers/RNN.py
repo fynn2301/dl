4ba98e1d57ac9a6d9e1abbc5e3c580d02959691a
Fynn-Lennardt Metzler <fynn-lennardt.metzler@fau.de>
2023-01-09 17:53:38 +0100

    feat: added empty rnn

--- Exercise3/src_to_implement/Layers/RNN.py
+++ Exercise3/src_to_implement/Layers/RNN.py
@@ -1,2 +1,33 @@
-class RNN():
-    pass
+from Layers.Base import BaseLayer
+import numpy as np
+class RNN(BaseLayer):
+    def __init__(self,input_size: int, hidden_size: int, output_size: int) -> None:
+        """Constructor
+
+        Args:
+            input_size (int): denotes the dimension of the input vector 
+            hidden_size (int): denotes the dimension of the hidden state
+            output_size (int): denotes the dimension of the output
+        """
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        
+        self.hidden_state = np.zeros((hidden_size))
+        
+        self.sub_layers = []
+        
+        super().__init__()
+        
+    def forward(self, input_tensor: np.array) -> np.array:
+        """forward propagation
+
+        Args:
+            input_tensor (np.array): input
+
+        Returns:
+            np.array: output
+        """
+        x_tilde = np.concatenate(input_tensor, self.hidden_state)
+        pass
+        
